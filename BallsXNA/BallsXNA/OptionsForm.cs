dab43d2d175ae9ce203913e8c284eb3cde777bc9
sergius2ch <sergius2ch@gmail.com>
2012-12-21 23:18:15 +0600

    first

---
+++ BallsXNA/BallsXNA/OptionsForm.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace BallsXNA
+{
+    public partial class OptionsForm : Form
+    {
+        public OptionsForm()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonOk_Click(object sender, EventArgs e)
+        {
+            DialogResult = DialogResult.OK;
+        }
+
+        private void buttonCancel_Click(object sender, EventArgs e)
+        {
+            DialogResult = DialogResult.Cancel;
+        }
+    }
+}
